3ab80bd769e9bd996ee591733854890699f692e9
olinkaz93 <ola.zieminska@gmail.com>
2018-10-16 13:58:23 +0100

    Koniec dzialu 6!

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
@@ -14,6 +15,7 @@ Navigation.registerComponent("travel-diary.AuthScreen", () => AuthScreen, store,
 Navigation.registerComponent("travel-diary.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent("travel-diary.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
 Navigation.registerComponent("travel-diary.PlaceDetailScreen", () => PlaceDetailScreen, store, Provider);
+Navigation.registerComponent("travel-diary.SideDrawer", () => SideDrawer);
 
 // Start a App
 Navigation.startSingleScreenApp({
