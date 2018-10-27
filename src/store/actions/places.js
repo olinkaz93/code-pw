094f2a88e6646510e1977e6c358968874931774d
olinkaz93 <ola.zieminska@gmail.com>
2018-10-27 16:21:18 +0100

    wrzucam obrazki do firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,25 +1,34 @@
-import { ADD_PLACE, DELETE_PLACE } from './actionTypes'
+import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
-//I store here my places related actions
 export const addPlace = (placeName, location, image) => {
-  return dispatch => {
-      const placeData = {
-          name: placeName,
-          location: location
-      };
-      fetch("https://traveldiaryokno-220216.firebaseio.com/places.json", {
-          method: "POST",
-          body: JSON.stringify(placeData)
-      })
-      .catch(err => console.log(err))
-      .then(res => res.json())
-      .then(parsedRes => {
-          console.log(parsedRes);
-      });
-  };  
+    return dispatch => {
+        fetch("https://us-central1-traveldiaryokno-220216.cloudfunctions.net/storeImage", {
+            method: "POST",
+            body: JSON.stringify({
+                image: image.base64
+            })
+        })
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            const placeData = {
+                name: placeName,
+                location: location,
+                image: parsedRes.imageUrl
+            };
+            return fetch("https://traveldiaryokno-220216.firebaseio.com/places.json", {
+                method: "POST",
+                body: JSON.stringify(placeData)
+            })
+        })  
+        .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(parsedRes => {
+            console.log(parsedRes);
+        });
+    };
 };
 
-//no argument needed
 export const deletePlace = (key) => {
     return {
         type: DELETE_PLACE,
